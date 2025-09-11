8ceff94e6fe0f1623891ab659809d08621a8b0ae
thomknoe <ciphermoon@yahoo.com>
2025-09-11 10:51:43 -0400

    Updated website

--- 01 html Template/script.js
+++
@@ -1,4 +0,0 @@
-// Cornell Tech Design Tech Project Template
-// Clean template - no JavaScript needed for basic scrolling website
-// Add any custom JavaScript functionality here as needed for your design tech project
-  
